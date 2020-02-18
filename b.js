55d1934163e767761a2a9d1402beacf954ceafa6
osneha <42754345+osneha@users.noreply.github.com>
2020-02-17 23:55:48 -0600

    change

--- b.js
+++ b.js
@@ -5,7 +5,7 @@ function b()
      console.log(Rate);
      const Tip=Total*(Rate/100)
      const Tax=Total*(5.5/100)
-     const Grandtotal=Total+Tip+Tax;
+     const GrandTotal=Total+Tip+Tax;
      document.querySelector('#tip').innerHTML=`$${Tip.toFixed(2)}`
      document.querySelector('#tax').innerHTML=`$${Tax.toFixed(2)}`
      document.querySelector('#grand-total').innerHTML=`$${GrandTotal.toFixed(2)}`
