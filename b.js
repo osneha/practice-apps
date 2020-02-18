6266414021bf80dce6ce187b23d8915dd7867c12
osneha <42754345+osneha@users.noreply.github.com>
2020-02-17 23:22:58 -0600

    few changes

--- b.js
+++ b.js
@@ -1,48 +1,13 @@
-const inputTotal = document.querySelector("#total")
-const inputRate =document.querySelector("#rate")
-const btnCalculate = document.getElementById("sumation")
-const calculationHold = document.getElementById("calcuation")
-const tipHold =document.getElementById("tip")
-const taxhold = document.getElementById("tax")
-
-  
-//gerneral call to functions and returning
-btnCalculate.onclick = async function(){
-    console.log('INFO: Declaring functions');
-    const total =inputTotal.value;
-    const rate =inputRate.value;
-    const totalNumber =parseInt(total);
-    const rateNumber =parseInt(rate);
-    //tip
-    const tipNum = await tip(totalNumber, rateNumber);
-    tipHold.innerHTML = "Tip: $"+tipNum;
-    //tax
-    const taxNum = await tax( totalNumber);
-    taxhold.innerHTML = "Tax: $"+taxNum;
-    // sum
-    const grand = await sum(totalNumber, tipNum, taxNum);
-    calculationHold.innerHTML = "Grand total: $"+grand;
+function logic()
+{
+     const Total=parseFloat(document.querySelector('#total').value)
+     const Rate=parseFloat(document.querySelector('#rate').value)
+     console.log(Rate);
+     const Tip=Total*(Rate/100)
+     const Tax=Total*(5.5/100)
+     const GrandTotal=Total+Tip+Tax;
+     document.querySelector('#tip').innerHTML=`$${Tip.toFixed(2)}`
+     document.querySelector('#tax').innerHTML=`$${Tax.toFixed(2)}`
+     document.querySelector('#grand-total').innerHTML=`$${GrandTotal.toFixed(2)}`
 }
-//found .fixed() at https://www.w3schools.com/jsref/jsref_tofixed.asp
-//tip calc
-const tip = async function ( totalNumber, rateNumber){
-  percent =rateNumber/100;
-  const giventip = totalNumber *percent;
-  return giventip.toFixed(2);
-
-}
-
-// tax calc
-const tax =async function (totalNumber){
-    const totaltax =(totalNumber * 0.055);
-    return totaltax.toFixed(2);
-}
-//sum calc
-const sum = async function (totalNumber, tipNum, taxNum){
-  // somehow they are stings
-    totalSum1 = parseFloat(totalNumber);
-    totalSum2 = parseFloat(tipNum)
-    totalSum3 = parseFloat(taxNum)
-    totalSum = totalSum1 + totalSum2 + totalSum3;
-    return totalSum.toFixed(2);
-}
+     document.querySelector('#btn').addEventListener('click',logic)
